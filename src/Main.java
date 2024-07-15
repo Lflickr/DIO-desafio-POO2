c2b3bebd6e05728a91396d5f56cebd7b2d808132
Italo <italo.bello10@gmail.com>
2024-07-15 12:23:03 -0300

    final

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -13,18 +15,44 @@ public class Main {
         curso1.setCargaHoraria(8);
 
         Curso curso2 = new Curso();
-        curso1.setTitulo("Curso JavaScript");
-        curso1.setDescricao("Descricao curso JavaScript");
-        curso1.setCargaHoraria(4);
+        curso2.setTitulo("Curso JavaScript");
+        curso2.setDescricao("Descricao curso JavaScript");
+        curso2.setCargaHoraria(4);
 
         Mentoria mentoria = new Mentoria();
         mentoria.setTitulo("Mentoria de Java");
         mentoria.setDescricao("Descricao mentoria Java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria);
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descricao Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devItalo = new Dev();
+        devItalo.setNome("Italo");
+        devItalo.inscreverBootcamp(bootcamp);
+        System.out.println("Italo Conteudos Intcritos:"+ devItalo.getConteudosInscritos());
+        System.out.println("Progressao realizada!!");
+        devItalo.progredir();
+        System.out.println("Progressao realizada!!");
+        devItalo.progredir();
+        System.out.println("Italo Conteudos Intcritos:"+ devItalo.getConteudosInscritos());
+        System.out.println("Italo Conteudos Concluidos:"+ devItalo.getConteudosConcluidos());
+        System.out.println("XP: "+ devItalo.calcularXp());
+
+        System.out.println("----------");
+
+        Dev devMaria = new Dev();
+        devMaria.setNome("Maria");
+        devMaria.inscreverBootcamp(bootcamp);
+        System.out.println("Progressao realizada!!");
+        devMaria.progredir();
+        System.out.println("Maria Conteudos Intcritos:"+ devMaria.getConteudosInscritos());
+        System.out.println("Maria Conteudos Concluidos:"+ devMaria.getConteudosConcluidos());
+        System.out.println("XP: "+ devMaria.calcularXp());
 
     }
 }
--- src/br/com/dio/desafio/dominio/Mentoria.java
+++ src/br/com/dio/desafio/dominio/Mentoria.java
@@ -2,19 +2,9 @@ package br.com.dio.desafio.dominio;
 
 import java.time.LocalDate;
 
-public class Mentoria {
+public class Mentoria extends Conteudo{
     //Atributos
-    private String titulo;
-    private String descricao;
     private LocalDate data;
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
 
     public LocalDate getData() {
         return data;
@@ -24,20 +14,17 @@ public class Mentoria {
         this.data = data;
     }
 
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
     @Override
     public String toString() {
         return "Mentoria{" +
-                "titulo='" + titulo + '\'' +
-                ", descricao='" + descricao + '\'' +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
                 ", data=" + data +
                 '}';
+    }
+
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO+20;
     }
 }
